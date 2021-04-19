3d50e07f7ad1df64463ff188d9408f6062f3c00e
imranbappy <imranhosenbappy690@gmail.com>
2021-04-20 05:10:44 +0600

    done

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // When the user scrolls the page, execute myFunction 
-            window.onscroll = function () { myFunction() };
-
-            function myFunction() {
-                var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-                var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-                var scrolled = (winScroll / height) * 100;
+            window.onscroll =  ()=> myFunction() ;
+            
+            const myFunction = _=> {
+                const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+                const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+                const scrolled = (winScroll / height) * 100;
                 document.getElementById("myBar").style.width = scrolled + "%";
             }
